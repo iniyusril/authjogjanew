f07bf0572601ea775d32101c17761071dc1ddda3
iniyusril <yusril.0503@students.amikom.ac.id>
2018-06-02 03:01:34 +0700

    update pertama

---
+++ js/script.js
@@ -0,0 +1,61 @@
+jQuery(document).ready(function(){
+// slider
+	"use strict";
+	$('#slider-carousel').carouFredSel({
+		responsive:true,
+		width:'100%;',
+		circular:true, 
+
+		scroll:
+			{
+				items:1,
+				duration:500,
+				pouseOnHover:true
+
+			},
+		auto:true,
+		items:
+
+				{
+					visible:
+					{
+						min:1,
+						max:1
+						},
+						height:"variable"
+					},
+ 
+				pagination:
+							{
+								container:".sliderpager",
+								pageAnchorBuilder:false
+							},
+	});
+
+
+
+
+// Akhir slider
+
+$(window).scroll(function(){
+	var top=$(window).scrollTop();
+
+	if (top>=60) 
+	{
+		$("header").addClass('secondary');
+
+	}
+
+	else if($("header").hasClass('secondary')){
+		$("header").removeClass('secondary');
+
+		} 
+
+	});
+
+
+$('#menu').slicknav({
+	label:'',
+	})
+
+});
